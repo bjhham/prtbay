e250d3d8cad00d6a39dda5239fb6c1a53be90b1f
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-02-12 14:19:42 +0100

    Add publications

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,11 +3,19 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
 }
 
 application {
     mainClass = "io.ktor.server.cio.EngineMain"
 }
+
+ktor {
+    fatJar {
+        archiveFileName = "prtbay-server.jar"
+    }
+}
+
 val copyWebDistToServerResources by tasks.registering(Copy::class) {
     dependsOn(project(":web").tasks.named("jsBrowserDistribution"))
     from(project(":web").layout.buildDirectory.dir("dist/js/productionExecutable"))
@@ -37,4 +45,37 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation(ktorLibs.server.testHost)
+}
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/bjhham/prtbay")
+            credentials {
+                username = System.getenv("GH_USER")
+                password = System.getenv("GH_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                url = "https://github.com/bjhham/prtbay"
+
+                licenses {
+                    license {
+                        name = "GNU General Public License, Version 3"
+                        url = "https://www.gnu.org/licenses/gpl-3.0.txt"
+                        distribution = "repo"
+                    }
+                }
+                scm {
+                    url = "https://github.com/bjhham/prtbay.git"
+                }
+            }
+        }
+    }
 }
